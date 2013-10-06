449560daffb737ad87787be82f7f24bdd1c7b902
Norman Cao <maplenormandy@gmail.com>
2013-10-06 07:01:31 -0400

    Moar improvements, on fire

--- Lightboard.js
+++ Lightboard.js
@@ -204,33 +204,41 @@ function calibrate() {
     lbc.fillStyle = 'black';
     lbc.clearRect(0, 0, 1, 1);
     
+    var cst = 0.1;
+    
+    if (calibFrame == 1) {
+      cst = 0.5;
+    } else if (calibFrame == 2) {
+      cst = 0.9;
+    }
+    
     lbc.fillStyle = '#006600';
-    lbc.fillRect(0.1-0.01125,0.0,0.0225,1.0);
-    lbc.fillRect(0.5-0.01125,0.0,0.0225,1.0);
-    lbc.fillRect(0.9-0.01125,0.0,0.0225,1.0);
+    lbc.fillRect(cst-0.01125,0.0,0.0225,1.0);
     
     lbc.fillStyle = '#660000';
-    lbc.fillRect(0.0,0.1-0.02,1.0,0.04);
-    lbc.fillRect(0.0,0.5-0.02,1.0,0.04);
-    lbc.fillRect(0.0,0.9-0.02,1.0,0.04);
-    
-    lbc.setTransform(1, 0, 0, 1, 0, 0);
+    lbc.fillRect(0.0,cst-0.02,1.0,0.04);
     
     console.log("ready 2");
     
-    setTimeout(calibrate, 600);
+    if (calibFrame == 0) {
+      setTimeout(calibrate, 600);
+    } else {
+      setTimeout(calibrate, 300);
+    }
     
     ++calibStage;
   } else if (calibStage == 3) {
     console.log("calibrate 3");
     
-    var sts = [0, 0, 0, 0, 0, 0];
-    var st = [0, 0, 0, 0, 0, 0];
-    var ss = [0, 0, 0, 0, 0, 0];
-    var stt = [0, 0, 0, 0, 0, 0];
-    var n = [0, 0, 0, 0, 0, 0];
-    slope = [0, 0, 0, 0, 0, 0];
-    inter = [0, 0, 0, 0, 0, 0];
+    if (calibFrame == 0) {
+      sts = [0, 0, 0, 0, 0, 0];
+      st = [0, 0, 0, 0, 0, 0];
+      ss = [0, 0, 0, 0, 0, 0];
+      stt = [0, 0, 0, 0, 0, 0];
+      n = [0, 0, 0, 0, 0, 0];
+      slope = [0, 0, 0, 0, 0, 0];
+      inter = [0, 0, 0, 0, 0, 0];
+    }
     
     var sampling = 4;
     
@@ -246,14 +254,11 @@ function calibrate() {
         y += 1;
       }
       
-      var yr = (y-minY)/(maxY-minY);
-      var xr = (x-minX)/(maxX-minX);
-      
       var red = img_data[ind*4];
       var green = img_data[ind*4+1]
       
       if (red > 250 && red > green + 10) {
-        var r = yr * 3 >>> 0;
+        var r = calibFrame;
         sts[r] += x*y;
         stt[r] += x*x;
         st[r] += x;
@@ -261,7 +266,7 @@ function calibrate() {
         n[r] += 1;
         //pData[0] = 255;
       } else if (green > 250 && green > red + 10) {
-        var r = (xr * 3 >>> 0) + 3;
+        var r = calibFrame + 3;
         sts[r] += x*y;
         stt[r] += y*y;
         st[r] += y;
@@ -275,88 +280,95 @@ function calibrate() {
       ind += -Math.log(Math.random())*sampling >>> 0;
     }
     
-    for (var i = 0; i < 6; ++i) {
-      slope[i] = (sts[i] - st[i]*ss[i]/n[i])/(stt[i] - st[i]*st[i]/n[i]);
-      inter[i] = (ss[i] - slope[i]*st[i])/n[i];
+    if (calibFrame == 2) {
+      lbc.setTransform(1, 0, 0, 1, 0, 0);
       
-      
-      lbc.beginPath();
-      if (i < 3) {
-        lbc.moveTo(0, inter[i]);
-        lbc.lineTo(width, inter[i]+slope[i]*width);
-      } else {
-        lbc.moveTo(inter[i], 0);
-        lbc.lineTo(inter[i]+slope[i]*height, height);
+      for (var i = 0; i < 6; ++i) {
+        slope[i] = (sts[i] - st[i]*ss[i]/n[i])/(stt[i] - st[i]*st[i]/n[i]);
+        inter[i] = (ss[i] - slope[i]*st[i])/n[i];
+        
+        lbc.beginPath();
+        if (i < 3) {
+          lbc.moveTo(0, inter[i]);
+          lbc.lineTo(width, inter[i]+slope[i]*width);
+        } else {
+          lbc.moveTo(inter[i], 0);
+          lbc.lineTo(inter[i]+slope[i]*height, height);
+        }
+        lbc.lineWidth = 3;
+        
+        lbc.strokeStyle = '#ffffff';
+        lbc.stroke();
+        
       }
-      lbc.lineWidth = 3;
       
-      lbc.strokeStyle = '#ffffff';
-      lbc.stroke();
+      inv = [[0,0,0],[0,0,0],[0,0,0]];
       
-    }
-    
-    inv = [[0,0,0],[0,0,0],[0,0,0]];
-    
-    for (var x1 = 0; x1 < 2; ++x1) {
-      for (var x2 = x1+1; x2 < 3; ++x2) {
-        for (var y1 = 0; y1 < 2; ++y1) {
-          for (var y2 = y1+1; y2 < 3; ++y2) {
-            /*
-            var yiv = (inter[y2]-inter[y1])/(slope[y1]-slope[y2]);
-            var xiv = inter[y1]+slope[y1]*yiv;
-            
-            var xiw = (inter[x2]-inter[x1])/(slope[x1]-slope[x2]);
-            var yiw = inter[x1]+slope[x1]*xiw;
-            
-            var ps = matrixRight(invertMatrix2([[xiv, yiv], [xiw, yiw]]), [-1,-1]);
-            
-            var proj = [[1,0,0],[0,1,0],[ps[0],ps[1],1]];
-            */
-            
-            var yi0 = (slope[x1]*inter[y1+3]+inter[x1])/
-                (1-slope[x1]*slope[y1+3]);
-            var xi0 = yi0*slope[y1+3]+inter[y1+3];
-            //var v0 = matrixRight(proj,[xi0, yi0, 1]);
-            var v0 = [xi0, yi0, 1];
-            //divideVec(v0, v0[2]);
-            
-            var yix = (slope[x1]*inter[y2+3]+inter[x1])/
-                (1-slope[x1]*slope[y2+3]);
-            var xix = yix*slope[y2+3]+inter[y2+3];
-            //var vx = matrixRight(proj,[xix, yix, 1]);
-            var vx = [xix, yix, 1];
-            //divideVec(vx, vx[2]);
-            
-            var yiy = (slope[x2]*inter[y1+3]+inter[x2])/
-                (1-slope[x2]*slope[y1+3]);
-            var xiy = yiy*slope[y1+3]+inter[y1+3];
-            //var vy = matrixRight(proj,[xiy, yiy, 1]);
-            var vy = [xiy, yiy, 1];
-            //divideVec(vy, vy[2]);
-            
-            var matlT = [[0.1+y1*0.4, 0.1+x1*0.4, 1],
-                        [0.1+y2*0.4, 0.1+x1*0.4, 1],
-                        [0.1+y1*0.4, 0.1+x2*0.4, 1]];
-            var matrT = invertMatrix3([v0, vx, vy]);
-            var coefs = matrixMult(matrT, matlT);
-            tMatrix3(coefs);
-            divideMatrix(coefs,coefs[2][2]);
-            
-            matrixSum(inv, coefs);
+      for (var x1 = 0; x1 < 2; ++x1) {
+        for (var x2 = x1+1; x2 < 3; ++x2) {
+          for (var y1 = 0; y1 < 2; ++y1) {
+            for (var y2 = y1+1; y2 < 3; ++y2) {
+              /*
+              var yiv = (inter[y2]-inter[y1])/(slope[y1]-slope[y2]);
+              var xiv = inter[y1]+slope[y1]*yiv;
+              
+              var xiw = (inter[x2]-inter[x1])/(slope[x1]-slope[x2]);
+              var yiw = inter[x1]+slope[x1]*xiw;
+              
+              var ps = matrixRight(invertMatrix2([[xiv, yiv], [xiw, yiw]]), [-1,-1]);
+              
+              var proj = [[1,0,0],[0,1,0],[ps[0],ps[1],1]];
+              */
+              
+              var yi0 = (slope[x1]*inter[y1+3]+inter[x1])/
+                  (1-slope[x1]*slope[y1+3]);
+              var xi0 = yi0*slope[y1+3]+inter[y1+3];
+              //var v0 = matrixRight(proj,[xi0, yi0, 1]);
+              var v0 = [xi0, yi0, 1];
+              //divideVec(v0, v0[2]);
+              
+              var yix = (slope[x1]*inter[y2+3]+inter[x1])/
+                  (1-slope[x1]*slope[y2+3]);
+              var xix = yix*slope[y2+3]+inter[y2+3];
+              //var vx = matrixRight(proj,[xix, yix, 1]);
+              var vx = [xix, yix, 1];
+              //divideVec(vx, vx[2]);
+              
+              var yiy = (slope[x2]*inter[y1+3]+inter[x2])/
+                  (1-slope[x2]*slope[y1+3]);
+              var xiy = yiy*slope[y1+3]+inter[y1+3];
+              //var vy = matrixRight(proj,[xiy, yiy, 1]);
+              var vy = [xiy, yiy, 1];
+              //divideVec(vy, vy[2]);
+              
+              var matlT = [[0.1+y1*0.4, 0.1+x1*0.4, 1],
+                          [0.1+y2*0.4, 0.1+x1*0.4, 1],
+                          [0.1+y1*0.4, 0.1+x2*0.4, 1]];
+              var matrT = invertMatrix3([v0, vx, vy]);
+              var coefs = matrixMult(matrT, matlT);
+              tMatrix3(coefs);
+              divideMatrix(coefs,coefs[2][2]);
+              
+              matrixSum(inv, coefs);
+            }
           }
         }
       }
+      
+      divideMatrix(inv, inv[2][2]);
+      console.log("done 3");
+      ++calibStage;
+    } else {
+      console.log("repeat 3");
+      --calibStage;
+      ++calibFrame;
     }
     
-    divideMatrix(inv, inv[2][2]);
-    
-    console.log("done 3");
-    
     setTimeout(calibrate, 80);
-    
-    ++calibStage;
   } else if (calibStage == 4) {
     console.log("clear 4");
+    
+    
     lbc.clearRect(0, 0, lightboard.width, lightboard.height);
     
     setTimeout(calibrate, 300);
